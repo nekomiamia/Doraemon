ffefaa0da5d54a3b0ebf14530e31252fba5a8e34
lid <407520408@qq.com>
2023-06-24 18:11:45 +0800

    feat: 单例基类以及事件中心

--- Assets/Scripts/Framework/Singleton.cs
+++ Assets/Scripts/Framework/Singleton.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public class Singleton<T> where T : class, new()
 {
-    // Start is called before the first frame update
-    void Start()
+    private static T instance;
+
+    public static T Instance
     {
-        
+        get
+        {
+            if (instance == null)
+            {
+                instance = new T();
+            }
+            return instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected Singleton()
     {
-        
     }
 }
